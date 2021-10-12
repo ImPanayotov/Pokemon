e6597b3ec4b9cfc0368ee538ee3016709b3fcd16
ivo.panayotov <ivo.panayotov@mentormate.com>
2021-10-12 18:27:15 +0300

    Creates welcome page
    User can update Pokemon list, show specific Pokemon or list all of them

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :pokemons, only: %i[index show update_pokemon_list]
   # root to: 'pokemons#index'
   get '/update-pokemons', to: 'pokemons#update_pokemon_list', defaults: { format: 'json' }
+  root to: 'welcome#index'
 end
