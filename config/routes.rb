98ece81828c43f3d29b678f5857fac8bfa327a4d
ivo.panayotov <ivo.panayotov@mentormate.com>
2021-10-12 12:24:26 +0300

    CHanges routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pokemons, only: %i[index show]
-  root to: 'pokemons#index'
-  get '/update-pokemons', to: 'pokemons#update_pokemon_list'
+  resources :pokemons, only: %i[index show update_pokemon_list]
+  # root to: 'pokemons#index'
+  # get '/update-pokemons', to: 'pokemons#update_pokemon_list'
 end
